88b4ce4327e9c28c2980d752adfa475e540daa3e
zweiein <evangelinathanasiakittymcdowell@gmail.com>
2014-12-11 14:19:44 +0800

    Can get user input now
    still has some bug in it.
    
    The speed of searching from Wordnet is slower than javascrript’s
    res.render().

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,77 @@
-var express = require('express')
-  , partials = require('express-partials')
-  , app = express();
+var express = require('express');
+var router = express.Router();
+var app = express();
 
-// load the express-partials middleware
-app.use(partials());
+var bodyParser = require('body-parser');
 
-function TextReader(){
-  console.log("Hello from TextReader");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-} // TextReader()
+// searching wordNet...
+var userSearch = 'network';
+var wordnetDatas = [];
 
-app.get('/',function(req,res,next){
-  res.render('index.ejs') 
-  // -> render layout.ejs with index.ejs as `body`.
-})
+function WordInfo(crossline, synsetOffset, pos, lemma, synonyms, gloss){
+// assign values to object
+  this.synsetOffset = synsetOffset;
+  this.crossline = crossline;
+  this.pos = pos;
+  this.lemma = lemma;
+  this.gloss = gloss;
+  this.synonyms = synonyms;
+} // function WordInfo()
+
+
+function SearchingWordNet( userSearchString ) {
+ console.log('start searching...');
+  var natural = require('natural');
+  var wordnet = new natural.WordNet('/usr/local/lib/node_modules/WNdb/dict');
+  var i = 0 ;
+  wordnetDatas = [];
+
+  wordnet.lookup(String(userSearch), function(results) {
+      results.forEach(function(result) {
+        var one_wordInfo = new WordInfo(result.synsetOffset, 
+                                        result.pos,
+                                        result.lemma,
+                                        result.synonyms,
+                                        result.gloss
+    	                                  );
+        //json = one_wordInfo.toJSONString();
+        //wordnetDatas[i] = JSON.parse(json);
+        wordnetDatas[i]  = one_wordInfo;
+        //wordnetDatas.push(one_wordInfo);
+        i++;
+      });
+      return wordnetDatas;
+  });
+} // SearchingWordNet()
+/*
+router.post('/', function(req, res){
+	userSearch = req.body.userSearchString;
+    console.log(req.body.userSearchString);
+
+    
+    res.render('index', { title: 'NTNU Bioinformatics courses',
+    	                  wordnetDatas: wordnetDatas });
+});*/
+
+SearchingWordNet('network');
+
+/* GET home page. */
+router.get('/',function(req,res){
+	console.log('router.get: @index.js');
+	SearchingWordNet(userSearch);
+	console.log(userSearch);
+	console.log(wordnetDatas);
+    res.render('index', { title: 'NTNU Bioinformatics courses',
+    	                  wordnetDatas: wordnetDatas,
+    	                  targetStr : 'Please Enter the keyword you want to search...' });
+    wordnetDatas = [];
+});
+
+
+module.exports = router;
 
